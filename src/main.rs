de1b158eac922a445378cf78cbd6fcab12d8e7fa
FlareFlo <mail@flareflo.dev>
2024-09-11 15:22:00 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env::args;
+use std::fs;
+use prost::Message;
+use srep::GameRecording;
+
+fn main() {
+	let path =  args().nth(1).expect("No Path passed");
+	// let input_dir = fs::read_dir(&path).unwrap().into_iter().collect();
+
+
+	// Example binary stream (Protobuf-encoded data)
+	let bin_data = fs::read(&path).unwrap();
+
+	// Decode the binary stream into the struct
+	let message = GameRecording::decode(bin_data.as_ref()).expect("Failed to decode Protobuf message");
+
+	dbg!(&message);
+}
